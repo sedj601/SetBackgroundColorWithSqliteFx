3e53618a5623b6618f59b422294abf28b6b6caf9
Sedrick Jefferson <sedrick.work@gmail.com>
2024-03-28 14:03:42 -0500

    Updates
    Updates

--- src/main/java/com/mycompany/setbackgroundcolorwithsqlitefx/App.java
+++ src/main/java/com/mycompany/setbackgroundcolorwithsqlitefx/App.java
@@ -2,11 +2,14 @@ package com.mycompany.setbackgroundcolorwithsqlitefx;
 
 import java.util.Map;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -19,9 +22,22 @@ public class App extends Application {
         Model model = new Model();//Use update properties as they change!
         Map<String, String> properties = model.getAllProperties();//Use to set initial properties only!
         
+        CheckBox checkBox = new CheckBox("Set Full Screen");
+        checkBox.setSelected(properties.get("FULL_SCREEN").equals("YES"));//Set initial CheckBox value.
+        checkBox.selectedProperty().addListener((obs, oldBoolean, newBoolean) -> {
+            if(newBoolean != null)
+            {
+                stage.setFullScreen(newBoolean);
+            }
+        });
+        
         ColorPicker colorPicker = new ColorPicker();
         colorPicker.setValue(Utility.DecodeColor(properties.get("BACKGROUND_COLOR")));//Set initial ColorPicker color.
-        StackPane root = new StackPane(colorPicker);
+        
+        VBox vBox = new VBox(checkBox, colorPicker);
+        vBox.setAlignment(Pos.CENTER);
+        
+        StackPane root = new StackPane(vBox);       
         root.setBackground(new Background(new BackgroundFill(Utility.DecodeColor(properties.get("BACKGROUND_COLOR")), null, null)));//Set initial background color.
         
         Scene scene = new Scene(root, 640, 480);        
